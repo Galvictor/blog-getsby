65d72ccf754ce5c323dc88c89c87f785fe7cd2d1
jlopes <joao.victor@lojainterativa.com>
2024-10-31 14:26:58 -0300

    ajustes no nodejs

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,9 +1,9 @@
-import { GatsbyNode } from 'gatsby';
+import {GatsbyNode} from 'gatsby';
 import path from 'path';
-import { posts } from './src/data/posts';
+import {posts} from './src/data/posts';
 
-export const createPages: GatsbyNode['createPages'] = async ({ actions }) => {
-    const { createPage } = actions;
+export const createPages: GatsbyNode['createPages'] = async ({actions}) => {
+    const {createPage} = actions;
 
     const postTemplate = path.resolve('src/templates/post.tsx');
 
@@ -16,4 +16,19 @@ export const createPages: GatsbyNode['createPages'] = async ({ actions }) => {
             },
         });
     });
+};
+
+export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] = ({actions}) => {
+    const {createTypes} = actions;
+    createTypes(`
+        type MdxFrontmatter {
+            thumb: File @fileByRelativePath
+        }
+        type File implements Node {
+            childImageSharp: ImageSharp
+        }
+        type ImageSharp implements Node {
+            gatsbyImageData: JSON
+        }
+    `);
 };
